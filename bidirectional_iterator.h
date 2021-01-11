c4000a2752ab5b74a5129a7e294e68ae97a97531
Qsymond <logvinov.evgeniy@ya.ru>
2021-01-11 19:28:10 +0300

    bidirectional_iterator.h fixed

--- bidirectional_iterator.h
+++ bidirectional_iterator.h
@@ -18,6 +18,9 @@ namespace ft {
     private:
         list_t<T> *_ptr;
     public:
+        typedef typename std::allocator<T>::reference reference;
+        typedef typename std::allocator<T>::pointer pointer;
+
         explicit bidirectional_iterator(void) : _ptr(nullptr) {};
 
         explicit bidirectional_iterator(list_t<T> *ptr) : _ptr(ptr) {};
@@ -32,12 +35,12 @@ namespace ft {
         virtual ~bidirectional_iterator() {};
 
         bidirectional_iterator &operator++() {
-            _ptr++;
+            _ptr = _ptr->next;
             return (*this);
         }
 
         bidirectional_iterator &operator--() {
-            _ptr--;
+            _ptr = _ptr->prev;
             return (*this);
         }
 
@@ -49,9 +52,9 @@ namespace ft {
             return (_ptr != bidirectionalIterator._ptr);
         };
 
-        T &operator*() { return *(_ptr->data); };
+        reference operator*() const { return (*_ptr->data); };
 
-        T *operator->() { return (_ptr->data); };
+        pointer operator->() const { return (_ptr->data); };
 
         list_t<T> *getCell() const { return (_ptr); }
     };
