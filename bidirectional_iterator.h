a038d7b7f98952b3e4e0cad319357ef49529ca1f
Qsymond <logvinov.evgeniy@ya.ru>
2021-01-11 20:03:28 +0300

    bidirectional_iterator.h fixed

--- bidirectional_iterator.h
+++ bidirectional_iterator.h
@@ -21,22 +21,29 @@ namespace ft {
         typedef typename std::allocator<T>::reference reference;
         typedef typename std::allocator<T>::pointer pointer;
 
-        explicit bidirectional_iterator(void) : _ptr(nullptr) {};
+        explicit bidirectional_iterator(void) : _ptr(nullptr) {}
 
-        explicit bidirectional_iterator(list_t<T> *ptr) : _ptr(ptr) {};
+        explicit bidirectional_iterator(list_t<T> *ptr) : _ptr(ptr) {}
 
-        bidirectional_iterator(const bidirectional_iterator &bidirectionalIterator) { *this = bidirectionalIterator; };
+        bidirectional_iterator(bidirectional_iterator const &bidirectionalIterator) : _ptr(
+                bidirectionalIterator._ptr) {}
 
         bidirectional_iterator &operator=(bidirectional_iterator const &bidirectionalIterator) {
             _ptr = bidirectionalIterator._ptr;
             return (*this);
         };
 
-        virtual ~bidirectional_iterator() {};
+        virtual ~bidirectional_iterator() {}
 
         bidirectional_iterator &operator++() {
             _ptr = _ptr->next;
             return (*this);
+        }
+
+        bidirectional_iterator operator++(int) {
+            bidirectional_iterator it(_ptr);
+            _ptr = _ptr->next;
+            return (it);
         }
 
         bidirectional_iterator &operator--() {
@@ -44,17 +51,23 @@ namespace ft {
             return (*this);
         }
 
+        bidirectional_iterator operator--(int) {
+            bidirectional_iterator it(_ptr);
+            _ptr = _ptr->next;
+            return (it);
+        }
+
         bool operator==(bidirectional_iterator const &bidirectionalIterator) {
             return (_ptr == bidirectionalIterator._ptr);
-        };
+        }
 
         bool operator!=(bidirectional_iterator const &bidirectionalIterator) {
             return (_ptr != bidirectionalIterator._ptr);
-        };
+        }
 
-        reference operator*() const { return (*_ptr->data); };
+        reference operator*() const { return (*_ptr->data); }
 
-        pointer operator->() const { return (_ptr->data); };
+        pointer operator->() const { return (_ptr->data); }
 
         list_t<T> *getCell() const { return (_ptr); }
     };
